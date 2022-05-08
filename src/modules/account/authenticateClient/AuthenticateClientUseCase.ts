92faf4fdf6bcdbb6b12f0fb3aa378c1eb1e583ed
skira <13675790+lschiavini@users.noreply.github.com>
2022-05-08 08:29:27 -0300

    ✨feature: Authenticate users

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,36 @@
+import { compare } from "bcrypt"
+import { sign } from "jsonwebtoken"
+import { prisma } from "../../../database/prismaClient"
+
+interface IAuthenticateClient {
+    username: string
+    password: string
+}
+
+export class AuthenticateClientUseCase {
+    async execute({ username, password }: IAuthenticateClient) {
+        const client = await prisma.client.findFirst({
+            where: {
+                username: username
+            }
+        })
+
+        if (!client) {
+            throw new Error("Username or password invalid")
+        }
+
+        const passMatch = await compare(password, client.password)
+
+        if (!passMatch) {
+            throw new Error("Username or password invalid")
+        }
+
+        const token = sign({ username }, "dac9630aec642a428cd73f4be0a03569", {
+            subject: client.id,
+            expiresIn: "1d"
+        })
+
+        return token
+
+    }
+};
