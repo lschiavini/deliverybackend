5079bf17debad74a5b55ef035d0ff27ad71f300c
skira <13675790+lschiavini@users.noreply.github.com>
2022-05-07 22:20:12 -0300

    ✨feature: Client usecase

---
+++ src/modules/client/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,32 @@
+import { prisma } from "../../../../database/prismaClient"
+import { hash } from 'bcrypt'
+
+interface ICreateClient {
+    username: string,
+    password: string
+}
+
+
+export class CreateClientUseCase {
+    async execute({ password, username }: ICreateClient) {
+        const clientExists = await prisma.client.findUnique({
+            where: {
+                username
+            }
+        })
+        if (clientExists) {
+            throw new Error("Client already exists")
+        }
+
+        const hashPassword = await hash(password, 10)
+        const client = await prisma.client.create({
+            data: {
+                username,
+                password: hashPassword
+            }
+        })
+
+        return client
+    }
+}
+
