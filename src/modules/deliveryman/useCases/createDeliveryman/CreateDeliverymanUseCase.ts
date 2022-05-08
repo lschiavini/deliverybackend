66a247389a890d8ac65933d2a152d3b4908d6000
skira <13675790+lschiavini@users.noreply.github.com>
2022-05-08 09:16:10 -0300

    ✨feature: CreateDeliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,33 @@
+import { compare, hash } from "bcrypt"
+import { prisma } from "../../../../database/prismaClient"
+
+interface ICreateDeliveryman {
+    username: string
+    password: string
+}
+
+export class CreateDeliverymanUseCase {
+ async execute({ username, password }: ICreateDeliveryman) {
+        const deliverymanExists = await prisma.deliveryman.findFirst({
+            where: {
+                username: username
+            }
+        })
+
+        if (deliverymanExists) {
+            throw new Error("Deliveryman already exists")
+        }
+
+        const hashPassword = await hash(password, 10)
+
+        const deliveryman = await prisma.deliveryman.create({
+            data: {
+                username,
+                password: hashPassword
+            }
+        })
+
+        return deliveryman
+
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express"
-import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/useCases/AuthenticateClientController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController"
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 
+const createDeliverymanController = new CreateDeliverymanController()
+
+
 routes.post("/client/", createClientController.handle)
+routes.post("/deliveryman/", createDeliverymanController.handle)
 routes.post("/authenticate/", authenticateClientController.handle)
 
 export { routes }
