1dcea99516ff1b71c405d83d0b2bbb907eb9122b
skira <13675790+lschiavini@users.noreply.github.com>
2022-05-09 11:11:09 -0300

    ✨feature: find available deliveries

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/useCases/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController"
 import { CreateDeliveryController } from "./modules/delivery/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/delivery/useCases/findAllAvailable/FindAllAvailableController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router()
@@ -15,12 +16,17 @@ const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 
 const createDeliveryController = new CreateDeliveryController()
+const findAllAvailableController = new FindAllAvailableController()
 // const authenticateDeliveryController = new AuthenticateDeliveryController()
 
 routes.post("/client/", createClientController.handle)
-routes.post("/deliveryman/", createDeliverymanController.handle)
-routes.post("/delivery/", ensureAuthenticateClient, createDeliveryController.handle)
 routes.post("/client/authenticate/", authenticateClientController.handle)
+
+routes.post("/deliveryman/", createDeliverymanController.handle)
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle)
+
+
+routes.post("/delivery/", ensureAuthenticateClient, createDeliveryController.handle)
+routes.get("/delivery/available", findAllAvailableController.handle)
 
 export { routes }
